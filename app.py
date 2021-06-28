d8723029d71352589658d226a3c766da5477978e
Tung <s1f102100029@iniad.org>
2021-06-28 16:53:21 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,20 @@
 from flask import Flask, render_template, request
 app = Flask(__name__)
 
+kinoko_count = 1
+takenoko_count = 1
+messages = ['Kinoko is wonderful!', 'Takenoko is awesome']
+
+
 @app.route('/')
 def top():
     return render_template('index.html', **vars())
 
 @app.route('/vote', methods=['POST'])
 def answer():
+    global kinoko_count, takenoko_count
+    kinoko_percent = kinoko_count / (kinoko_count + takenoko_count) * 100
+    takenoko_percent = takenoko_count / (kinoko_count + takenoko_count) * 100
     return render_template('vote.html', **vars())
 
 if __name__ == '__main__':
